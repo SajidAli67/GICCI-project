3d7307c049412c1167f15e7aff5b6339c6b7c4a0
sajid Ali <sajid.ali67@outlook.com>
2022-02-20 23:39:45 +0500

    create all dashboar pages only one page is remaning in dashboard

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,62 +1,9 @@
 $(document).ready(function(){
-    $('#all').click(function(){
-        $('#web-development-work').fadeIn(1000);
-        $('#web-design-work').fadeIn(1000);
-        $('#graphic-design-work').fadeIn(1000);
-        $('#photo-graphy-work').fadeIn(1000);
-        $('#seo-work').fadeIn(1000);
-        $('#market-work').fadeIn(1000);
+    $('#aside-show').click(function(){
+        $('#aside_bar').addClass('show-aside')
+        console.log('...')
     })
-
-    $('#web-development').click(function(){
-        $('#web-development-work').fadeIn(1000);
-        $('#web-design-work').fadeOut(1000);
-        $('#graphic-design-work').fadeOut(1000);
-        $('#photo-graphy-work').fadeOut(1000);
-        $('#seo-work').fadeOut(1000);
-        $('#market-work').fadeOut(1000);
-    })
-
-    $('#web-design').click(function(){
-        $('#web-development-work').fadeOut(1000);
-        $('#web-design-work').fadeIn(1000);
-        $('#graphic-design-work').fadeOut(1000);
-        $('#photo-graphy-work').fadeOut(1000);
-        $('#seo-work').fadeOut(1000);
-        $('#market-work').fadeOut(1000);
-    })
-
-    $('#graphic-design').click(function(){
-        $('#web-development-work').fadeOut(1000);
-        $('#web-design-work').fadeOut(1000);
-        $('#graphic-design-work').fadeIn(1000);
-        $('#photo-graphy-work').fadeOut(1000);
-        $('#seo-work').fadeOut(1000);
-        $('#market-work').fadeOut(1000);
-    })
-
-    $('#photo-graphy').click(function(){
-        $('#web-development-work').fadeOut(1000);
-        $('#web-design-work').fadeOut(1000);
-        $('#graphic-design-work').fadeOut(1000);
-        $('#photo-graphy-work').fadeIn(1000);
-        $('#seo-work').fadeOut(1000);
-        $('#market-work').fadeOut(1000);
-    })
-    $('#seo').click(function(){
-        $('#web-development-work').fadeOut(1000);
-        $('#web-design-work').fadeOut(1000);
-        $('#graphic-design-work').fadeOut(1000);
-        $('#photo-graphy-work').fadeOut(1000);
-        $('#seo-work').fadeIn(1000);
-        $('#market-work').fadeOut(1000);
-    })
-    $('#market').click(function(){
-        $('#web-development-work').fadeOut(1000);
-        $('#web-design-work').fadeOut(1000);
-        $('#graphic-design-work').fadeOut(1000);
-        $('#photo-graphy-work').fadeOut(1000);
-        $('#seo-work').fadeOut(1000);
-        $('#market-work').fadeIn(1000);
+    $('#aside-close').click(function(){
+        $('#aside_bar').removeClass('show-aside')
     })
 });

a1ebdca7c3944a9abc991d7a90ddedb49ef30bfd
sajid Ali <sajid.ali67@outlook.com>
2022-02-22 01:22:51 +0500

    add business detail pages create

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,3 +7,35 @@ $(document).ready(function(){
         $('#aside_bar').removeClass('show-aside')
     })
 });
+
+
+  $(document).ready(function () {
+            var current_fs, next_fs, previous_fs; //fieldsets
+            var opacity;
+            $(".next").click(function () {
+                current_fs = $(this).parent().parent();
+                 next_fs = $(this).parent().parent().next();
+                $("#progressbar li").eq($("fieldset").index(next_fs)).addClass("active-progress");
+                next_fs.show();
+                current_fs.animate({ opacity: 0 }, {
+                    step: function (now) {
+                        opacity = 1 - now;
+                        current_fs.css({
+                            'display': 'none',
+                            'position': 'relative'
+                        });
+                        next_fs.css({ 'opacity': opacity });
+                    },
+                    duration: 600
+                });
+            });
+        })
+
+//preview img
+    var loadFile = function(event) {
+    var output = document.getElementById('output');
+    output.src = URL.createObjectURL(event.target.files[0]);
+    output.onload = function() {
+      URL.revokeObjectURL(output.src) // free memory
+    }
+  };

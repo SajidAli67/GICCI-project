6a0d6b0030b090b5282197a4d1d9b0e8d61d71a8
sajid Ali <sajid.ali67@outlook.com>
2022-02-27 00:53:43 +0500

    update

--- assets/lib/owl/owl.js
+++ assets/lib/owl/owl.js
@@ -5,16 +5,17 @@
           item:3,
           autoplay:true,
           autoplayHoverPause:true,
-          nav:true,
+          nav:false,
+          margin: 20,
           responsive:{
                   0:{
                       items:1, 
                   },
                   600:{
-                      items:1,
+                      items:2,
                   },
                   1000:{
-                      items:3,
+                      items:4,
                       loop:true
                   }
               }
